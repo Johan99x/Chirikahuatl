1d1c0c3b88acac5970ab39e7506f9dc4c037037f
Johan99x <39012068+Johan99x@users.noreply.github.com>
2018-05-07 22:56:30 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Clan Chirikahuatl</title>
+
+	<link rel="stylesheet" href="css/index_style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src='js/script.js'></script>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<a href=''><li>Inicio</li></a>
+				<a href=''><li>Proyectos</li></a>
+				<a href=''><li>Im&aacute;genes</li></a>
+			</ul>
+				
+				<img id='logo' src='pictures/logo.png'>
+
+			<ul>
+				<a href=''><li>Nosotros</li></a>
+				<a href=''><li>Contacto</li></a>
+				<a href='indexreg.html'><li>Registrate</li></a>
+			</ul>
+		</nav>
+	</header>
+
+	<section id='banner'>
+		
+	</section>
+
+	<section id='body'></section>
+</body>
+</html>
